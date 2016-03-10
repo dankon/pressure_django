7704f56e21c20756a2afb3ae497159d24be69906
dankon <dankarol@MKSAMTF14.desy.de>
2016-03-10 17:15:10 +0100

    First admin working version

---
+++ pressure/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+
+class BloodPressure(models.Model):
+    author = models.ForeignKey('auth.User')
+    systolic_value = models.IntegerField()
+    diastolic_value = models.IntegerField()
+    pulse =  models.IntegerField()
+    comment = models.TextField()
+    add_date_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.add_date_time)
