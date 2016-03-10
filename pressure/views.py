5288f8f0f0b7520e4e8e64d8b3b8798cffb995c5
dankon <dankarol@MKSAMTF14.desy.de>
2016-03-10 21:12:49 +0100

    Table of pressure

--- pressure/views.py
+++ pressure/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import BloodPressure
 
 def pressure_list(request):
-    return render(request, 'pressure/pressure_list.html', {})
+    all_blood_pressure = BloodPressure.objects.all().order_by('-add_date_time')
+    return render(request, 'pressure/pressure_list.html', 
+                  {'all_bl_press' : all_blood_pressure})
